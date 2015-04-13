ce3941f97241921c6a43f159c6cf6464c2627507
root <ibwood@indiana.edu>
2015-04-13 16:23:33 +0000

    withGetResults

--- getResults.sh
+++ getResults.sh
@@ -1,5 +1,10 @@
-declare -a counts=("tweetCount", "DrugCount", "SymptomCount", "coDrugCount", "coDrugSymptomCount", "coCoDrugSymptomCount")
-for folder in "${counts[@]}":
+declare -a counts=("tweetCount" "DrugCount" "SymptomCount" "coDrugCount" "coDrugSymptomCount" "coCoDrugSymptomCount")
+for folder in "${counts[@]}"
 do
-    hadoop fs -get /lists/"$folder" ./results/
+    echo "Getting $folder"
+    hadoop fs -get /lists/$folder ./results/
+    echo "Sending Results to File"
+    mv ./results/$folder/part-r-00000 ./results/$folder.csv
+    echo "Cleaning"
+    rm -rf ./results/$folder
 done
